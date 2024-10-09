cf5b7ff9dc85017ed3cd98133321534a451570e4
FlappyBird <gui09bg@gmail.com>
2024-10-09 11:45:27 -0300

    flap

---
+++ FlappyBirdy/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FlappyBirdy;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FlappyBirdy/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace FlappyBirdy;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
