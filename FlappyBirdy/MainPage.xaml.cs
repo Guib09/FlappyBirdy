c64703ab888fcab7815412254a7d9fe8019a867d
FlappyBirdy <gui09bg@gmail.com>
2024-10-11 09:47:28 -0300

    flapc

--- FlappyBirdy/MainPage.xaml.cs
+++ FlappyBirdy/MainPage.xaml.cs
@@ -2,13 +2,15 @@
 
 public partial class MainPage : ContentPage
 {
-
+	
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	
+ private void irjogar(object sender, EventArgs args)
+	{
+		Application.Current.MainPage = new GamePage();
 }
-
+}
---
+++ GamePage.xaml.cs
@@ -0,0 +1,30 @@
+namespace FlappyBirdy
+partial class GamePage : ContentPage
+{
+const int Gravidade=5;
+const int TempoEntreFrames=40;
+bool EstarMorto=false;
+
+    public GamePage()
+	{
+		InitializeComponent();
+	}
+	void AplicaGravidade()
+	{
+		navelol.TranslationY +=Gravidade;
+	}
+	
+
+     async Task Desenhar()
+	 {
+       while (!EstarMorto)
+       {
+		AplicaGravidade();
+		await Task.Delay(TempoEntreFrames);
+	   }
+	 }
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		Desenhar();
+    }
