48f8a5516e141ffd72018d687b679cba756dc330
FlappyBirdy <gui09bg@gmail.com>
2024-10-11 10:58:58 -0300

    flap b

--- FlappyBirdy/MainPage.xaml.cs
+++ FlappyBirdy/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace FlappyBirdy;
+﻿using AVFoundation;
+using MapKit;
+using UIKit;
+
+namespace FlappyBirdy;
 
 public partial class MainPage : ContentPage
 {
@@ -12,5 +16,27 @@ public partial class MainPage : ContentPage
  private void irjogar(object sender, EventArgs args)
 	{
 		Application.Current.MainPage = new GamePage();
-}
+		Double LarguraJanela=0;
+		Double AlturaJanela=0;
+		int Velocidade=0;
+
+    protected override void OnSizeAllocated(double w, double h)
+    {
+        base OnSizeAllocated(w,h);
+		LarguraJanela=w;
+		AlturaJanela=h;
+
+		void GerenciaCanos()
+		{
+			imgCanoCima.TranslationX-=velocidade;
+			imgCanoBaixo.TranslationX-=velocidade;
+			if(IMKGeoJsonObject CanoBaixo.TranslationX<=LarguraJanela)
+			{
+				imgCanobaixo.Translation.TranslationX=0;
+				imgCanoCima.TranslationX=0;
+			}
+		}
+	 GerenciaCanos()
+	}
+	}
 }
